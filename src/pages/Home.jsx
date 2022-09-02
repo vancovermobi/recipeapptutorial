de06f7e23681c7b19bc3a9ddb42bd8c0dd904bbc
vancovermobi <vancovermobi@gmail.com>
2022-09-02 16:48:14 +0700

    Origin

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Veggie from '../component/Veggie'
+import Popular from '../component/Popular'
+
+function Home() {
+  return (
+    <div>
+        <Veggie />
+        <Popular />
+    </div>
+  )
+}
+
+export default Home
