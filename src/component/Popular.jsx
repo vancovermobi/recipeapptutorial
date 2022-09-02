ab553f1084b1875278fc451f5c48220f454b5005
vancovermobi <vancovermobi@gmail.com>
2022-09-02 23:09:09 +0700

    Fetch-Foot-api

--- src/component/Popular.jsx
+++ src/component/Popular.jsx
@@ -1,5 +1,23 @@
+import { useEffect } from "react"
 
 function Popular() {
+const urlApi = "https://api.spoonacular.com/recipes/random"
+const apiKey = process.env.REACT_APP__FOOT_API_KEY
+const numberPages = 10
+
+useEffect(() =>{
+    getPopular()
+}, [])
+
+const getPopular = async () =>{
+    const api = await fetch(
+       `${urlApi}?apiKey=${apiKey}&number=${numberPages}`
+       //`https://api.spoonacular.com/recipes/random?apiKey=fe406a9023774e89b6448ea11c5b97b5&number=9`
+       )
+    const data = await api.json()
+    console.log("data", data);
+}
+
   return (
     <div>Popular</div>
   )
